d8df5d6f259e203ae6af8dcd1ee20f72e8896839
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2021-03-01 23:37:02 +0000

    lombok

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
+public class Car {
+
+    private int year;
+    private int door;
+    private String model;
+    private String make;
+    private long phoneNumber ;
+
+}
